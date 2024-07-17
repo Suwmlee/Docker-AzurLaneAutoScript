84553525c090e99016d9bd7f218790aca9e42600
Mathhew <suwmlee@gmail.com>
2024-07-17 20:49:30 +0800

    minor fixes

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ EXPOSE 22267
 
 # Install dependencies
 RUN apt-get update && \
-    apt-get install -y netcat-traditional unzip
+    apt-get install -y git adb libgomp1
 
 # update TZ
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,12 @@ if [ ! -f "$TEMPLATE_FILE" ];then
     cp /app/config/* /app/AzurLaneAutoScript/config/
 fi
 
-ERROR=$( { $PYROOT/bin/python3 -m mxnet | sed s/Output/Useless/ > outfile; } 2>&1 )
+ERROR=$( { python -m mxnet | sed s/Output/Useless/ > outfile; } 2>&1 )
 if [[ ! $ERROR =~ "module" ]]; then
     echo "try to fix mxnet version"
-    $PYROOT/bin/pip install --upgrade mxnet
+    pip install --upgrade mxnet
 fi
 
 # run alas
-$PYROOT/bin/python -m uiautomator2 init && \
-$PYROOT/bin/python /app/AzurLaneAutoScript/gui.py
+python -m uiautomator2 init && \
+python /app/AzurLaneAutoScript/gui.py
